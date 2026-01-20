f1da5f72b5740d52fdc24977307f70b0a5af32af
Thota-bharat999 <bharatmanithota@gmail.com>
2026-01-20 16:43:16 +0530

    cors error

--- src/utilis/socket.js
+++ src/utilis/socket.js
@@ -2,15 +2,8 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createScoketConnection = () => {
-  if (location.hostname === "localhost") {
-    // Local development
-    return io(BASE_URL, {
-      withCredentials: true,
-    });
-  } else {
-    // Production
-    return io("https://devtinder-1-xdb9.onrender.com", {
-      withCredentials: true,
-    });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+    transports: ["websocket", "polling"],
+  });
 };
